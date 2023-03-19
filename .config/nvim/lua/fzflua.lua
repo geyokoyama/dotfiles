a4150593c731416ccbef6c5c2541dea776ff0dc7
geyokoyama <george_y@outlook.com>
2023-03-19 09:02:51 -0700

    Change keybinding for FzfLua

--- .config/nvim/lua/fzflua.lua
+++ .config/nvim/lua/fzflua.lua
@@ -1,4 +1,4 @@
 -- [[ fzflua.lua ]] --
 
 vim.keymap.set('n','<c-p>', ':FzfLua files<CR>', { silent = true })
-vim.keymap.set('n','<Leader>b', ':FzfLua buffers<CR>', { silent = true })
+vim.keymap.set('n','<c-b>', ':FzfLua buffers<CR>', { silent = true })
