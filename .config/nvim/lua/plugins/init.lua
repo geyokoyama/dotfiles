baec86622aa19b5f667cef03053870e9fe1371c1
geyokoyama <george_y@outlook.com>
2023-10-08 14:36:47 -0700

    Neovim Config: Add Neogit

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -88,15 +88,23 @@ require("lazy").setup({
     config = function() require("lualine").setup() end
   },
 
-  "sindrets/diffview.nvim",
-
   {
     "jiaoshijie/undotree",
     dependencies = "nvim-lua/plenary.nvim",
     config = true,
-    keys = { -- load the plugin only when using it's keybinding:
+    keys = {
       { "<F4>", "<cmd>lua require('undotree').toggle()<cr>" },
     },
+  },
+
+  {
+    "NeogitOrg/neogit",
+    dependencies = {
+      "nvim-lua/plenary.nvim",         -- required
+      "sindrets/diffview.nvim",        -- optional
+      "ibhagwan/fzf-lua",              -- optional
+    },
+    config = true
   }
 
 })
