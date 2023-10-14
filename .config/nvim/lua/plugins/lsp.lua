c90c1dba4bd33766ef484de918a5b5a504a6cfd9
geyokoyama <george_y@outlook.com>
2023-10-14 10:00:34 -0700

    Neovim: Add keymap for formatting

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -58,5 +58,8 @@ vim.api.nvim_create_autocmd("LspAttach", {
     vim.keymap.set("n", "gl", vim.diagnostic.open_float, opts)
     vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
     vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
+    vim.keymap.set('n', '<F5>', function()
+      vim.lsp.buf.format { async = true }
+    end, opts)
   end
 })
